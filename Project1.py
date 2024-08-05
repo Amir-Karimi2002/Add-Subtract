0ca8be9e49bb6d93ae49a1b43996c86410170202
AmirHossein <mr.amirhossein138181@gmail.com>
2024-08-05 13:17:02 +0200

    sum and sub

---
+++ Project1.py
@@ -0,0 +1,11 @@
+num1 = int(input("Please Enter first number: "))
+num2 = int(input("Please Enter second number: "))
+
+# Sum
+sum = num1 + num2
+
+# Subtract
+Sub = num1 - num2
+
+print("The sum equals to:", sum)
+print("The subtract equals to:", Sub)
